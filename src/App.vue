e900d576e41b0d8f6240ad1ecea608db4bda3e1b
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-17 18:23:58 +0800

    12/17

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script></script>
+
+<style scoped lang="stylus">
+  @import "~common/stylus/variable";
+
+  #app
+    color: $color-theme
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="text">Chicken Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import "~common/stylus/variable"
+    @import "~common/stylus/mixin"
+    
+
+</style>
